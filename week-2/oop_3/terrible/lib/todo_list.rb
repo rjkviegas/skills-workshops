96cee717b908d642632086034cb899c344a9f0ca
Mary Rose Cook <mary@maryrosecook.com>
2017-01-11 09:17:40 +0000

    Consistently use to_string rather than to_s

--- week-2/oop_3/terrible/lib/todo_list.rb
+++ week-2/oop_3/terrible/lib/todo_list.rb
@@ -13,7 +13,7 @@ class TodoList
 
   def to_string
     all.each_with_index.map { |todo, index|
-      todo_to_s(todo, index + 1)
+      todo_to_string(todo, index + 1)
     }.join("\n")
   end
 
@@ -27,13 +27,13 @@ class TodoList
     @todos
   end
 
-  def todo_to_s(todo, index)
+  def todo_to_string(todo, index)
     description = todo[:description]
-    complete = todo_completion_to_s(todo)
+    complete = todo_completion_to_string(todo)
     "#{index}. #{description} #{complete}"
   end
 
-  def todo_completion_to_s(todo)
+  def todo_completion_to_string(todo)
     todo[:complete] ? "complete" : "not complete"
   end
 end
