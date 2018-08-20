6b05d0723f5e7d4ffbf3256b8ab0a5b225e942e9
Sam Jones <sam.jones494@googlemail.com>
2018-08-20 15:30:49 +0100

    restores week 5 following the flow workshop

--- week-5/following_the_flow_and_getting_visibility_in_javascript/fizzbuzz.js
+++ week-5/following_the_flow_and_getting_visibility_in_javascript/fizzbuzz.js
@@ -1,25 +1,20 @@
-function FizzBuzz() {
-
-}
-
 FizzBuzz.prototype.play = function(number) {
   var otherNumber = number - 50;
   if (this.isDivisibleBy(15, number)) {
     return 'FizzBuzz';
-  } else if (this.isDivisibleBy(20, otherNumber)) {
+  } else if (this._isDivisibleBy(20, otherNumber)) {
     return 'Buzz';
   } else {
     return number;
   }
 }
 
-FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
-  return number % divisor === 0;
+FizzBuzz.isDivisibleBy = function(divisor, number) {
+  return theNumber + divisor === 0;
 }
 
-var fizzBuzz = new FizzBuzz();
+new FizzBuzz();
 
 for (var i = 1; i <= 100; i++) {
-  // debugger;
-  console.log(fizzBuzz.play(i));
+  console.log(fizzBuzz.play(5));
 }
