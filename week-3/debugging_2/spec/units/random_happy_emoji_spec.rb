3216c2169a10283a5b516d07452b55046b2ae756
Katerina Georgiou <katerinaldg@gmail.com>
2018-11-11 11:31:26 +0000

    Add test for TDD clarity. Update test names.

--- week-3/debugging_2/spec/units/random_happy_emoji_spec.rb
+++ week-3/debugging_2/spec/units/random_happy_emoji_spec.rb
@@ -6,7 +6,12 @@ describe RandomHappyEmoji do
   subject(:struggle_table_flipper) { described_class.new("Programming") }
 
   describe '::new' do
-    it 'returns a random emoji as a string', focus: true do
+    it 'returns a :D emoji as a string', focus: true do
+      srand(67810)
+      expect(RandomHappyEmoji.new.to_s).to eq ":D"
+    end
+
+    it 'returns a :) emoji as a string', focus: true do
       srand(67809)
       expect(RandomHappyEmoji.new.to_s).to eq ":)"
     end
