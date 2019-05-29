74c12d12f11a56b10f2dfc565f6e10486fc71bd1
lindsey <lindsey@makers.tech>
2019-05-29 14:25:05 +0100

    Change variable names in controller

--- week-3/debugging_2/lib/app.rb
+++ week-3/debugging_2/lib/app.rb
@@ -3,14 +3,15 @@ require_relative "./struggle_table_flipper"
 require_relative "./random_happy_emoji"
 
 class StruggleTableFlipperApp < Sinatra::Base
+
   get "/emoji" do
-    erb :index, { locals: { emoji: RandomHappyEmoji.new } }
+    @emoji =  RandomHappyEmoji.new
+    erb :index
   end
 
   get "/flipped_struggle" do
-    flipped_struggle = StruggleTableFlipper.new(params[:the_struggle]).flipped_struggle
-
-    erb :flipped_struggle, { locals: { flipped_struggle: flipped_struggle } }
+    @flipped_struggle = StruggleTableFlipper.new(params[:the_struggle]).flipped_struggle
+    erb :flipped_struggle
   end
 
   run! if app_file == $0
