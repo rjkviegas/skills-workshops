b35ba6367467559fb59bc5349a0e40151fd160a7
Josh Hill <josh.hill@makers.tech>
2019-09-24 17:51:29 +0100

    Fix todo spec

--- ruby-fundamentals/class-syntax/exercise3/spec/todo_spec.rb
+++ ruby-fundamentals/class-syntax/exercise3/spec/todo_spec.rb
@@ -5,10 +5,12 @@ describe Todo do
   it "is an instance of the Todo class" do
     todo = Todo.new("get milk")
 
-    expect(todo).to be_instance_of(TodoList)
+    expect(todo).to be_a(Todo)
   end
 
   it "has a description" do
-    expect(Todo.new("get milk").description).to eq("get milk")
+    todo = Todo.new("get milk")
+
+    expect(todo.description).to eq("get milk")
   end
 end
