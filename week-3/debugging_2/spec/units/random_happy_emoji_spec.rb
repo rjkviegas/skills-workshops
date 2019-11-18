3d500d9dd07f1dd591633537e62fc03f3b871c93
Josh Hill <josh.hill@makers.tech>
2019-11-18 14:21:53 +0000

    Remove unused subject from debugging_2 unit test

--- week-3/debugging_2/spec/units/random_happy_emoji_spec.rb
+++ week-3/debugging_2/spec/units/random_happy_emoji_spec.rb
@@ -3,8 +3,6 @@
 require 'random_happy_emoji'
 
 describe RandomHappyEmoji do
-  subject(:struggle_table_flipper) { described_class.new("Programming") }
-
   describe '::new' do
     it 'returns a :D emoji as a string', focus: true do
       srand(67810)
