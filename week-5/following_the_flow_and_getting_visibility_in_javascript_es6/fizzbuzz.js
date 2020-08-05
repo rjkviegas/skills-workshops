013e2dd2a430079c5d2d94ab2655bea447d2089d
Ryan Viegas <rjkviegas@gmail.com>
2020-08-05 10:11:51 +0100

    following flow and getting visibility workshop

--- week-5/following_the_flow_and_getting_visibility_in_javascript_es6/fizzbuzz.js
+++ week-5/following_the_flow_and_getting_visibility_in_javascript_es6/fizzbuzz.js
@@ -1,22 +1,23 @@
 class FizzBuzz {
   play(number) {
-    let otherNumber = number - 50;
     if (this._isDivisibleBy(15, number)) {
       return 'FizzBuzz';
-    } else if (this._isDivisibleBy(20, otherNumber)) {
+    } else if (this._isDivisibleBy(3, number)) {
+      return 'Fizz';
+    } else if (this._isDivisibleBy(5, number)) {
       return 'Buzz';
     } else {
       return number;
     }
   }
 
-  isDivisibleBy(divisor, number) {
-    return theNumber + divisor === 0;
+  _isDivisibleBy(divisor, number) {
+    return number % divisor === 0;
   }
 }
 
-new FizzBuzz();
+var fizzBuzz = new FizzBuzz();
 
 for (let i = 1; i <= 100; i++) {
-  console.log(fizzBuzz.play(5));
+  console.log(fizzBuzz.play(i));
 }
